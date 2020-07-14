7d46a6d4127286849c6ffc4ee935ecc5f199b07c
Jerrrryc <7786424+jerrrryc@user.noreply.gitee.com>
2020-07-14 13:04:59 +0800

    7/14 update

--- app/src/main/java/com/cq/xinyupintai/view/MainView.java
+++ app/src/main/java/com/cq/xinyupintai/view/MainView.java
@@ -1,16 +1,133 @@
 package com.cq.xinyupintai.view;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.cq.xinyupintai.R;
 
-public class MainView extends AppCompatActivity {
+public class MainView extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv1;
+    private EditText editinput;
+    private EditText etpassword;
+    private boolean isHideFirst = true;
+    private ImageView inputclear;
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (editinput.getEditableText().length() >= 1) {
+                inputclear.setVisibility(View.VISIBLE);
+            } else {
+                inputclear.setVisibility(View.GONE);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initview();
+        initListerner();
+
+
+        tv1 = findViewById(R.id.newuser);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainView.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+        etpassword = (EditText) findViewById(R.id.password);
+
+        final Drawable[] drawables = etpassword.getCompoundDrawables();
+        final int eyeWidth = drawables[2].getBounds().width();// 眼睛图标的宽度
+        final Drawable drawableEyeOpen = getResources().getDrawable(R.mipmap.open_eye);
+
+        drawableEyeOpen.setBounds(drawables[2].getBounds());//这一步不能省略
+
+        etpassword.setOnTouchListener(new View.OnTouchListener() {
+
+                                          @Override
+                                          public boolean onTouch(View v, MotionEvent event) {
+
+
+                                              if (event.getAction() == MotionEvent.ACTION_UP) {
+                                                  float et_pwdMinX = v.getWidth() - eyeWidth - etpassword.getPaddingRight();
+                                                  float et_pwdMaxX = v.getWidth();
+                                                  float et_pwdMinY = 0;
+                                                  float et_pwdMaxY = v.getHeight();
+                                                  float x = event.getX();
+                                                  float y = event.getY();
+
+                                                  if (x < et_pwdMaxX && x > et_pwdMinX && y > et_pwdMinY && y < et_pwdMaxY) {
+                                                      // 点击了眼睛图标的位置
+                                                      isHideFirst = !isHideFirst;
+
+                                                      if (isHideFirst) {
+                                                          etpassword.setCompoundDrawables(null, null, drawables[2], null);
+                                                          etpassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+                                                      } else {
+                                                          etpassword.setCompoundDrawables(null, null, drawableEyeOpen, null);
+                                                          etpassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                                                      }
+                                                  }
+                                              }
+                                              return false;
+                                          }
+
+                                      }
+
+        );
+    }
+
+    private void initview() {
+        inputclear = findViewById(R.id.clearn);
+        editinput = findViewById(R.id.user);
+
+    }
+
+    private void initListerner() {
+        editinput.addTextChangedListener(textWatcher);
+        inputclear.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.clearn:
+                editinput.setText("");
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/cq/xinyupintai/view/register.java
@@ -0,0 +1,13 @@
+package com.cq.xinyupintai.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
