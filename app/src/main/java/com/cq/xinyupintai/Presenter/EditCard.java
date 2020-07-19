a995d0737be4e2b2267a2e08b0a820d77deeea2c
Jerrrryc <7786424+jerrrryc@user.noreply.gitee.com>
2020-07-20 01:05:02 +0800

    7/20 更新预付卡添加编辑

---
+++ app/src/main/java/com/cq/xinyupintai/Presenter/EditCard.java
@@ -0,0 +1,16 @@
+package com.cq.xinyupintai.Presenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cq.xinyupintai.R;
+
+public class EditCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_card);
+    }
+}
