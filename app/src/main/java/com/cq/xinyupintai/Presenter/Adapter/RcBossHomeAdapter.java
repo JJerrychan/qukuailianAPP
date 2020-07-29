cf65efe32338b27d58255e7a8d38c1067ebe8e61
LeePrince <leewangzi@outlook.com>
2020-07-29 20:15:46 +0800

    添加店铺数据统计功能

---
+++ app/src/main/java/com/cq/xinyupintai/Presenter/Adapter/RcBossHomeAdapter.java
@@ -0,0 +1,57 @@
+package com.cq.xinyupintai.Presenter.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cq.xinyupintai.R;
+
+import java.util.List;
+
+public class RcBossHomeAdapter extends RecyclerView.Adapter<RcBossHomeAdapter.TextHolder> {
+    private Context mContext;
+    private List<String> texts;
+    public RcBossHomeAdapter(Context context, List<String> textInfo){
+        mContext = context;
+        texts = textInfo;
+    }
+
+    @Override
+    public TextHolder onCreateViewHolder(ViewGroup parent, int viewType) {          //创建viewholder
+        View view = View.inflate(mContext,R.layout.item_boss,null);            //获取视图
+        return new TextHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TextHolder holder, int position) {                 //绑定数据
+        //后续获取数据需要修改此处
+        String mtext = texts.get(position);
+        holder.setnameText(mtext);
+        holder.setdataText("数据测试");
+    }
+
+    @Override
+    public int getItemCount() {                             //获取项目数量
+        return texts.size();
+    }
+
+    public static class TextHolder extends RecyclerView.ViewHolder{
+        private TextView name;
+        private TextView data;
+        public TextHolder(View itemView) {
+            super(itemView);
+            name=itemView.findViewById(R.id.tv_item_bh1);
+            data=itemView.findViewById(R.id.tv_item_bh2);
+        }
+        public void setnameText(String nameText){
+            name.setText(nameText);
+        }
+        public void setdataText(String dataText){
+            data.setText(dataText);
+        }
+    }
+}
--- app/src/main/java/com/cq/xinyupintai/Presenter/fragment/boss_home_fragment.java
+++ app/src/main/java/com/cq/xinyupintai/Presenter/fragment/boss_home_fragment.java
@@ -11,10 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+
+import com.cq.xinyupintai.Presenter.Adapter.RcBossHomeAdapter;
 import com.cq.xinyupintai.R;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 class messagePageAdapter extends FragmentPagerAdapter {
     private Context mContext;
@@ -69,11 +78,26 @@ public class boss_home_fragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         TabLayout tabLayout1=view.findViewById(R.id.tb_boss_1);
         ViewPager viewPager = view.findViewById(R.id.vp_boss_1);
+/*
+        RecyclerView rcvBossHome=view.findViewById(R.id.rc_boss_1);
+        rcvBossHome.setLayoutManager (new GridLayoutManager(getActivity(),3));
+        rcvBossHome.setAdapter(new RcBossHomeAdapter(getActivity(),TextInit()));
+ */
         //设置adapter
         viewPager.setAdapter(new messagePageAdapter(getChildFragmentManager(), tabLayout1.getTabCount()));
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout1));
         tabLayout1.setupWithViewPager(viewPager);
     }
+/*
+    private List<String> TextInit() {
+        List<String> text = new ArrayList<>();
+        text.add("收入");
+        text.add("使用人数");
+        text.add("售出数");
+        return text;
+    }
+ */
+
 
     public static boss_home_fragment newInstance(int index) {
         boss_home_fragment fragment = new boss_home_fragment();
