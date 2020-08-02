72726d29ced9960af44c6f58c2ec14d643156002
LeePrince <leewangzi@outlook.com>
2020-08-02 10:05:44 +0800

    增加回复弹出Toast和返回主功能界面

--- app/src/main/java/com/cq/xinyupintai/Presenter/activity/EvaActivity.java
+++ app/src/main/java/com/cq/xinyupintai/Presenter/activity/EvaActivity.java
@@ -3,16 +3,36 @@ package com.cq.xinyupintai.Presenter.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.cq.xinyupintai.R;
 import com.xuexiang.xui.XUI;
 
 public class EvaActivity extends AppCompatActivity {
-
+    private Button ensure;
+    private  Button cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         XUI.initTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eva);
+        ensure =findViewById(R.id.rb_require_ensure);
+        cancel=findViewById(R.id.rb_require_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ensure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast=Toast.makeText(EvaActivity.this,"回复成功",Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+            }
+        });
     }
 }
